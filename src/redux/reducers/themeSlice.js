03435cdfe12d4467053847c08c9727c67e352839
anastaxgr <alexanastagr@gmail.com>
2025-03-12 04:04:21 -0700

    localstorage settings

--- src/redux/reducers/themeSlice.js
+++ src/redux/reducers/themeSlice.js
@@ -1,22 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const getInitialTheme = () => {
+  const savedTheme = localStorage.getItem("theme");
+  return savedTheme ? JSON.parse(savedTheme) : { darkTheme: false, background: "bg1",language:"en" };
+};
+
 const themeSlice = createSlice({
   name: "theme",
-  initialState: {
-    darkTheme: false,
-    background: "#ffffff",
-  },
+  initialState: getInitialTheme(),
   reducers: {
     toggleDarkTheme: (state, action) => {
-      state.darkTheme =
-        action.payload !== undefined ? action.payload : !state.darkTheme;
+      state.darkTheme = action.payload !== undefined ? action.payload : !state.darkTheme;
+      localStorage.setItem("theme", JSON.stringify(state)); 
     },
 
     setBackground: (state, action) => {
       state.background = action.payload;
+      localStorage.setItem("theme", JSON.stringify(state));  
+    },
+
+    
+    setLanguage: (state, action) => {
+      state.language = action.payload;
+      localStorage.setItem("theme", JSON.stringify(state));  
     },
   },
 });
 
-export const { toggleDarkTheme, setBackground } = themeSlice.actions;
+export const { toggleDarkTheme, setBackground,setLanguage } = themeSlice.actions;
 export default themeSlice.reducer;
